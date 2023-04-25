c1f9467e09ecf50f0320e9b74b2c7f5d353f41e2
gabrielakoleva99 <gabriela.koleva991@gmail.com>
2023-04-25 15:00:08 +0200

    write main class to execute runnable class and according junit tests

---
+++ runmerunner/src/main/java/htwb/ai/Main.java
@@ -0,0 +1,13 @@
+package htwb.ai;
+
+//Main Class should find all declared methods in the class RunnableClass and list the
+//method names which are not annotated with @RunMe and which are annotated with @RunMe.
+//The methods annotated with @RunMe should be executed
+//If these methods throw exceptions during execution, the framework must indicate which method was not
+//executable and what error was thrown during execution.
+public class Main {
+    public static void main(String[] args){
+
+
+    }
+}
---
+++ runmerunner/src/test/java/htwb/ai/TestRunMeRunner.java
@@ -0,0 +1,46 @@
+package htwb.ai;
+
+import org.junit.jupiter.api.Test;
+
+public class TestRunMeRunner {
+
+
+
+    @Test
+    public void classNotInstantiatableShouldReturnFalse(){
+
+    }
+    @Test
+    public void emptyClassShouldReturnFalse(){
+
+    }
+
+    @Test
+    public void classIsNullShouldReturnFalse(){
+
+    }
+    @Test
+    public void classNameInvalidShouldReturnFalse(){
+
+    }
+    @Test
+    public void hasMethodsWithAnnotation(){
+
+    }
+    @Test
+    public void showMethodsWithAnnotationShouldReturnTrue(){
+
+    }
+
+    @Test
+    public void showMethodsWithoutAnnotationShouldReturnTrue(){
+
+    }
+
+    @Test
+    public void notExecutableMethodShouldThrowExceptionMessage(){
+
+    }
+
+
+}
