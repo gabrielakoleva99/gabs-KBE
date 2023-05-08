48656fc8a231d0094e132c62b761c2fdf7ccf10a
gabrielakoleva99 <gabriela.koleva991@gmail.com>
2023-05-09 01:03:13 +0200

    added more tests and cleaned mainprogram

--- runmerunner/src/test/java/htwb/ai/TestRunMeRunner.java
+++ runmerunner/src/test/java/htwb/ai/TestRunMeRunner.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class TestRunMeRunner {
@@ -48,7 +50,7 @@ public class TestRunMeRunner {
     public void nonInstantiatableClassFoundShouldReturnError() {
         outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
-        // Set the system property to point to a test class that doesn't exist
+        // Set the system property to point to a test class that cant be instantiated
         System.setProperty("classToRun", "java.io.Closeable");
 
         // Run the Main class
@@ -56,6 +58,25 @@ public class TestRunMeRunner {
 
         // Verify that the output contains the expected method names and error messages
         String expectedOutput = "Analyzed class 'java.io.Closeable':"+System.lineSeparator()+"Could not instantiate class java.io.Closeable"+System.lineSeparator()+"Usage: java -DclassToRun=your.package.ClassName -jar runmerunner-TEAMNAME.jar";
+
+        Assertions.assertEquals(expectedOutput.trim(), outContent.toString().trim());
+    }
+
+    @Test
+    public void nonInstantiatableClassShouldReturnExceptionError() {
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        // Set the system property to point to a test class that cant be instantiated
+        System.setProperty("classToRun", "java.lang.Number");
+
+        // Run the Main class
+        Main.main(null);
+
+        // Verify that the output contains the expected method names and error messages
+        String expectedOutput = "Analyzed class 'java.lang.Number':"
+                +System.lineSeparator()+"java.lang.Number: InstantiationException"
+                +System.lineSeparator()+
+                "Usage: java -DclassToRun=your.package.ClassName -jar runmerunner-TEAMNAME.jar";
 
         Assertions.assertEquals(expectedOutput.trim(), outContent.toString().trim());
     }
@@ -92,6 +113,34 @@ public class TestRunMeRunner {
 
         // Check that the correct number of methods with @RunMe are returned
         Assertions.assertEquals(2, runnableMethods.length);
+    }
+
+    @Test
+    public void testHasMethodsWithAnnotation() {
+        Class<Main> resourceClass = Main.class;
+        Method[] methods = resourceClass.getDeclaredMethods();
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(RunMe.class)) {
+                Assertions.assertEquals("RunMe", Arrays.toString(m.getAnnotations()));
+            }
+        }
+    }
+
+    @Test
+    public void testShowMethodsWithAnnotation() {
+        Class<Main> c = Main.class;
+        Method[] methods = c.getDeclaredMethods();
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(RunMe.class)) {
+                ArrayList<Method> methodsWithRunMe = new ArrayList<>();
+                methodsWithRunMe.add(m);
+                String s = "findme1" + System.lineSeparator() + "findme2" + System.lineSeparator()
+                        +"findme3" + System.lineSeparator() +"findme4" + System.lineSeparator()
+                        +"findme5" + System.lineSeparator() +"findme6" + System.lineSeparator();
+
+                Assertions.assertEquals(s, methodsWithRunMe.toString());
+            }
+        }
     }
 
 }
